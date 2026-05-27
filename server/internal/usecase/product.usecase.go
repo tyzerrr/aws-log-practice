f899ac168683f5d6a3a9b10e5fd4c575fdd39ab4
Taichi Araki <t-b-araki@mercari.com>
2026-05-28 07:53:26 +0900

    chore(usecase): introduce transaction manager

--- server/internal/usecase/product.usecase.go
+++ server/internal/usecase/product.usecase.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log/slog"
 
+	"github.com/tyzerrr/aws-log-practice/server/internal/adapter/db/sqlc"
+	"github.com/tyzerrr/aws-log-practice/server/internal/adapter/transaction"
 	"github.com/tyzerrr/aws-log-practice/server/internal/domain"
 	"github.com/tyzerrr/aws-log-practice/server/internal/domain/entity"
 )
@@ -12,21 +14,35 @@ type ProductUsecase interface {
 	ListProducts(ctx context.Context) ([]*entity.Product, error)
 }
 
+type ProductRepositoryFactory func(db sqlc.DBTX) domain.ProductRepository
+
 type productUsecase struct {
-	logger            *slog.Logger
-	productRepository domain.ProductRepository
+	logger    *slog.Logger
+	newRepo   ProductRepositoryFactory
+	txManager transaction.Tx
 }
 
 func NewProductUsecase(
 	logger *slog.Logger,
-	productRepository domain.ProductRepository,
+	newRepo ProductRepositoryFactory,
+	txManager transaction.Tx,
 ) ProductUsecase {
 	return &productUsecase{
-		logger:            logger,
-		productRepository: productRepository,
+		logger:    logger,
+		newRepo:   newRepo,
+		txManager: txManager,
 	}
 }
 
 func (uc *productUsecase) ListProducts(ctx context.Context) ([]*entity.Product, error) {
-	return nil, nil
+	// NOTE: memory allocation strategy is not good...
+	var products []*entity.Product
+	if err := uc.txManager.ReadOnlyTransaction(ctx, func(ctx context.Context, tx sqlc.DBTX) error {
+		var err error
+		products, err = uc.newRepo(tx).FindAll(ctx)
+		return err
+	}); err != nil {
+		return nil, err
+	}
+	return products, nil
 }
