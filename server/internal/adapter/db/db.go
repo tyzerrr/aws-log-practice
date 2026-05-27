8b76d4e2059329ece72cb30ce4bd9f055541e733
Taichi Araki <t-b-araki@mercari.com>
2026-05-28 03:05:08 +0900

    chore(db): add db pool and ping

---
+++ server/internal/adapter/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DBPool struct {
+	pool   *pgxpool.Pool
+	logger *slog.Logger
+}
+
+func NewDBPool(ctx context.Context, logger *slog.Logger, connectionString string) (*DBPool, error) {
+	p, err := pgxpool.New(ctx, connectionString)
+	if err != nil {
+		logger.Error("failed to initialize db pool", slog.String("error", err.Error()))
+		return nil, err
+	}
+	if err := p.Ping(ctx); err != nil {
+		p.Close()
+		logger.Error("failed to ping to db pool, so close db pool", slog.String("error", err.Error()))
+		return nil, err
+	}
+	return &DBPool{
+		pool:   p,
+		logger: logger,
+	}, nil
+}
+
+func (db *DBPool) Close() {
+	db.pool.Close()
+}
