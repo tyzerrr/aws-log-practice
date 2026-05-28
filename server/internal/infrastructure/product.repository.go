7d49a2f2b766a8ef9072f69a155bdaed955733f8
Taichi Araki <t-b-araki@mercari.com>
2026-05-29 04:03:53 +0900

    chore(repo): update repository impl

--- server/internal/infrastructure/product.repository.go
+++ server/internal/infrastructure/product.repository.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tyzerrr/aws-log-practice/server/internal/adapter/db/sqlc"
 	"github.com/tyzerrr/aws-log-practice/server/internal/domain"
 	"github.com/tyzerrr/aws-log-practice/server/internal/domain/entity"
+	"github.com/tyzerrr/aws-log-practice/server/internal/domain/valueobject"
 )
 
 type ProductRepository struct {
@@ -18,10 +19,46 @@ func NewProductRepository(db sqlc.DBTX) domain.ProductRepository {
 	}
 }
 
-func (r *ProductRepository) CreateOne(ctx context.Context, product *entity.Product) (*entity.Product, error) {
-	return nil, nil
+func (pr *ProductRepository) CreateOne(ctx context.Context, product *entity.Product) (*entity.Product, error) {
+	model, err := pr.querier.CreateProduct(ctx, sqlc.CreateProductParams{
+		Name:         product.Name.Value(),
+		Description:  product.Description,
+		PriceAmount:  product.PriceAmount.Value(),
+		CurrencyCode: product.Currency.Value(),
+		IsActive:     !product.Archived,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return restoreProduct(&model)
 }
 
 func (pr *ProductRepository) FindAllActiveProducts(ctx context.Context) ([]*entity.Product, error) {
 	return nil, nil
+}
+
+func restoreProduct(model *sqlc.Product) (*entity.Product, error) {
+	name, err := valueobject.NewProductName(model.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	priceAmount, err := valueobject.NewProductAmount(model.PriceAmount)
+	if err != nil {
+		return nil, err
+	}
+
+	currency, err := valueobject.NewCurrency(model.CurrencyCode)
+	if err != nil {
+		return nil, err
+	}
+
+	return entity.NewProduct(
+		model.ID.Bytes,
+		name,
+		model.Description,
+		priceAmount,
+		currency,
+		!model.IsActive,
+	), nil
 }
