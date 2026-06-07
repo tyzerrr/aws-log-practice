ce3e4db890ab9ce1acae71a489217d427fe91751
Taichi Araki <t-b-araki@mercari.com>
2026-06-07 20:30:04 +0900

    feat(batch): add aws client for batch

--- server/cmd/batch/aws.client.go
+++ server/cmd/batch/aws.client.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
+type awsClient struct {
+	smc    *secretsmanager.Client
+	logger *slog.Logger
+}
+
+func (c *awsClient) GetSecretValue(ctx context.Context, input *secretsmanager.GetSecretValueInput) ([]byte, error) {
+	result, err := c.smc.GetSecretValue(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	if result.SecretString == nil {
+		return nil, fmt.Errorf("secret string is empty")
+	}
+	return []byte(*result.SecretString), nil
+}
