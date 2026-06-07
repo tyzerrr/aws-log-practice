68bf52dfd075b9e2ae7d315e43e5aca8a9918144
Taichi Araki <t-b-araki@mercari.com>
2026-06-07 20:30:14 +0900

    chore(batch): add entry point for batch

--- server/cmd/batch/main.go
+++ server/cmd/batch/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	ExitOK int = iota
+	ExitErr
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	if err := realMain(ctx); err != nil {
+		fmt.Printf("Error: %v\n", err.Error())
+		os.Exit(ExitErr)
+	}
+	os.Exit(ExitOK)
+}
+
+func realMain(ctx context.Context) error {
+	if len(os.Args) != 2 {
+		return fmt.Errorf("usage: go run ${ROOT of aws-log-practice}/server/cmd/batch/ [create_db_app_user]")
+	}
+	var err error
+
+	switch os.Args[1] {
+	case "create_db_app_user":
+		err = CreateDBAppUser(ctx)
+	default:
+		err = fmt.Errorf("invalid command: %s", os.Args[1])
+	}
+
+	return err
+}
