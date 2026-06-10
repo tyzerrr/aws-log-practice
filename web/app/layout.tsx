e40cf71c96af0efbc4b4d5378c8af6899949f0d6
Taichi Araki <t-b-araki@mercari.com>
2026-06-11 00:24:42 +0900

    chore(layout): update layout using Header and Footer

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "AWS Log Practice",
@@ -12,8 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className="h-full antialiased">
-      <body className="flex min-h-full flex-col">{children}</body>
-    </html>
+      <>
+        <html lang="ja" className="h-screen antialiased">
+          <body className="flex min-h-screen flex-col">
+            <Header/>
+            <main className={"flex-1"}>
+                {children}
+            </main>
+            <Footer/>
+          </body>
+        </html>
+      </>
   );
 }
