60fd149ead4d564290edeaf2995279ba5ee061c4
Taichi Araki <t-b-araki@mercari.com>
2026-06-11 00:25:16 +0900

    chore(component): add Cart

---
+++ web/components/Cart.tsx
@@ -0,0 +1,9 @@
+import {ShoppingCart} from "lucide-react"
+
+export default function Cart() {
+    return (
+        <div>
+            <ShoppingCart className={"size-6"}/>
+        </div>
+    )
+}
