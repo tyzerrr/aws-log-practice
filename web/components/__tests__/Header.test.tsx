dcb03525775085cccda3df03126994ba6fb77673
Taichi Araki <t-b-araki@mercari.com>
2026-06-13 22:05:01 +0900

    chore(test): add test to confirm if Header is rendered with Cart, Account, Search correctly

--- web/components/__tests__/Header.test.tsx
+++ web/components/__tests__/Header.test.tsx
@@ -15,7 +15,7 @@ describe("Header", () => {
 
 describe("Header", () => {
   it("renders the likes to home", () => {
-    render(<Likes />);
+    render(<Header />);
     const likes = screen.getByRole("button", { name: "likes-button" });
     expect(likes).not.toBeNull();
   });
@@ -23,11 +23,27 @@ describe("Header", () => {
 
 describe("Header", () => {
   it("renders the search to home", () => {
-    render(<Search />);
+    render(<Header />);
     const searchInput = screen.getByRole("textbox", { name: "search-input" });
     expect(searchInput).not.toBeNull();
 
     const placeholder = screen.getByPlaceholderText("商品を検索...");
     expect(placeholder).not.toBeNull();
+  });
+});
+
+describe("Header", () => {
+  it("renders the account to home", () => {
+    render(<Header />);
+    const account = screen.getByRole("button", { name: "account-button" });
+    expect(account).not.toBeNull();
+  });
+});
+
+describe("Header", () => {
+  it("renders the cart to home", () => {
+    render(<Header />);
+    const cart = screen.getByRole("button", { name: "cart-button" });
+    expect(cart).not.toBeNull();
   });
 });
