80fe95d10e86e561f68e385b9f7e40978ce3a4f0
Taichi Araki <t-b-araki@mercari.com>
2026-06-13 22:06:02 +0900

    chore(component): update Cart component, adding aria-label to write unittest

--- web/components/Cart.tsx
+++ web/components/Cart.tsx
@@ -2,7 +2,7 @@ import { ShoppingCart } from "lucide-react";
 
 export default function Cart() {
   return (
-    <button type="button">
+    <button type="button" aria-label="cart-button">
       <ShoppingCart className={"size-6"} />
     </button>
   );
