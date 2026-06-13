28bbdc10b702ced043e098d6e428164f55273e28
Taichi Araki <t-b-araki@mercari.com>
2026-06-13 22:05:34 +0900

    chore(component): update Account, adding aria-label for writing unittest

--- web/components/Account.tsx
+++ web/components/Account.tsx
@@ -2,7 +2,7 @@ import { User } from "lucide-react";
 
 export default function Account() {
   return (
-    <button type="button">
+    <button type="button" aria-label="account-button">
       <User />
     </button>
   );
