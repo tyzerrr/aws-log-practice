dc71cea51ab918b888b79f33f81116febfab7b0c
Taichi Araki <t-b-araki@mercari.com>
2026-06-13 21:58:10 +0900

    chore(component): update Likes component

--- web/components/Likes.tsx
+++ web/components/Likes.tsx
@@ -2,7 +2,7 @@ import { Heart } from "lucide-react";
 
 export default function Likes() {
   return (
-    <button type="button">
+    <button type="button" aria-label="likes-button">
       <Heart />
     </button>
   );
